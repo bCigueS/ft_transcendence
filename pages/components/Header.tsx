7185d2e9eea369be401445fc18f80c5c673af97e
sbeylot <sbeylot@student.42.fr>
2023-04-11 18:51:02 +0200

    Adding Layout and styling

---
+++ pages/components/Header.tsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+	return (
+		
+		<div style={{
+			backgroundColor: "black",
+			width: '24px',
+			height: '24px',
+			}}>
+		</div>
+		
+	);
+}
+
+export default Header;
