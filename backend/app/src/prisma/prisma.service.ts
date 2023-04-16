0737e375cf0e6884354c9b9a20f3d4043e6ba218
olivwalsh <oliviawalsh073@gmail.com>
2023-04-16 21:09:53 +0200

    Add non working docker

--- backend/app/src/prisma/prisma.service.ts
+++ backend/app/src/prisma/prisma.service.ts
@@ -1,17 +1,11 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor(config: ConfigService) {
-        super({
-            datasources: {
-                db: {
-                    url :"postgresql://postgres:123@localhost:5432/postgres?schema=public"
-                    // url: config.get('DATABASE_URL')
-                }
-            }
-        })
-    }
+    async enableShutdownHooks(app: INestApplication) {
+		this.$on('beforeExit', async () => {
+		  await app.close();
+		});
+	  }
 }
