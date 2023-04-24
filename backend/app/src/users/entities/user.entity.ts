4a5a10971a2661d6f00fca2d4448c33b7268aeda
olivwalsh <oliviawalsh073@gmail.com>
2023-04-24 18:54:00 +0200

    Add user routes and authentication

--- backend/app/src/users/entities/user.entity.ts
+++ backend/app/src/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserEntity implements User {
+	@ApiProperty()
+	id: number;
+
+	@ApiProperty()
+	name: string;
+
+	@ApiProperty()
+	email: string;
+
+	@ApiProperty()
+	password: string;
+
+	@ApiProperty({ required: false })
+	avatar: string;
+
+	@ApiProperty({ required: false })
+	friends: User[]
+
+	@ApiProperty({ required: false })
+	blocked: User[]
+	
+}
