e5ee495b636f48874927e5d0371bac9b8d6a45e7
owalsh <owalsh@student.42.fr>
2023-05-02 20:29:22 +0200

    :construction: Add route to add friends in user

--- backend/app/src/users/users.controller.ts
+++ backend/app/src/users/users.controller.ts
@@ -5,10 +5,12 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddFriendDto } from './dto/add-friend.dto';
 
 @Controller('users') @ApiTags('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService, private prisma: PrismaService) {}
 
 	@Post()
 	@ApiCreatedResponse({ type: UserEntity })
@@ -17,16 +19,16 @@ export class UsersController {
 	}
 
 	@Get()
-	@UseGuards(JwtAuthGuard)
-	@ApiBearerAuth()
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
 	@ApiOkResponse({ type: UserEntity, isArray: true })
 	findAll() {
     	return this.usersService.findAll();
   }
 
 	@Get(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiBearerAuth()
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
 	@ApiOkResponse({ type: UserEntity })
 	async findOne(@Param('id', ParseIntPipe) id: number) {
 		const user = await this.usersService.findOne(id);
@@ -36,8 +38,8 @@ export class UsersController {
   }
   
 	@Patch(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiBearerAuth()
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
 	@ApiOkResponse({ type: UserEntity })
 	async update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
 		const user = await this.usersService.findOne(id);
@@ -47,12 +49,40 @@ export class UsersController {
 	}
 	
 	@Delete(':id')
-	@UseGuards(JwtAuthGuard)
-	@ApiBearerAuth()
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
 	@ApiOkResponse({ type: UserEntity })
 	remove(@Param('id', ParseIntPipe) id: number) {
     	return this.usersService.remove(id);
 	}
 
+	@Patch(':id/add-friend')
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
+	@ApiOkResponse({ type: UserEntity })
+	async addFriend(@Param('id', ParseIntPipe) id: number, @Body() addFriendDto: AddFriendDto) {
+		const user = await this.usersService.findOne(id);
+		if (!user)
+			throw new NotFoundException(`User with ${id} does not exist.`);
+
+		const friendId = addFriendDto.friendId;
+		const friend = await this.usersService.findOne(friendId);
+		if (!friend)
+			throw new NotFoundException(`Friend with ${friendId} does not exist.`);
+
+		return this.usersService.addFriend(id, friendId);
+	}
+
+	@Get(':id/show-friends')
+	// @UseGuards(JwtAuthGuard)
+	// @ApiBearerAuth()
+	@ApiOkResponse({ type: UserEntity })
+	async showFriends(@Param('id', ParseIntPipe) id: number) {
+		const user = await this.usersService.findOne(id);
+		if (!user)
+			throw new NotFoundException(`User with ${id} does not exist.`);
+
+		return this.usersService.showFriends(id);
+	}
 
 }
