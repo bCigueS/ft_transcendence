3f40155da64c97b947708f6d0ca2ed9b6f572f5a
sbeylot <sbeylot@student.42.fr>
2023-05-03 17:59:30 +0200

    Finish settings pannel

--- frontend/src/components/Profile/ProfilIcon.tsx
+++ frontend/src/components/Profile/ProfilIcon.tsx
@@ -2,17 +2,42 @@ import React from 'react';
 import { User } from '../../store/users-contexte';
 import classes from '../../sass/components/Profile/ProfilIcon.module.scss';
 
-const ProfilIcon: React.FC<{user: User}> = ( props ) => {
+
+
+const ProfilIcon: React.FC<{user: User; displayCo?: boolean; size?: string[]}> = ( { user, displayCo = true, size = []}) => {
+
+	const stylePicture: React.CSSProperties = {
+		content: '',
+		position: 'absolute',
+		inset: '-0.7rem',
+		background: 'linear-gradient(90deg, rgba(245,173,167,1) 0%, rgba(241,93,81,1) 35%, rgba(235,137,71,1) 100%)',
+		borderRadius: '50%'
+	};
+
 	return (
-		<div className={classes.profilPic}>
-			<div className={classes.picture}>
-				<img src={props.user.profilePic} alt={props.user.nickname} />
+		<div 
+			className={classes.profilPic} 
+			style={size.length > 0 ? {width: size[0], height: size[1]} : {}}>
+			
+			{ size.length > 0 &&
+				<div style={stylePicture}></div>
+			}
+			<div 
+				className={classes.picture}
+				style={size.length > 0 ? {width: size[0], height: size[1] } : {}}>
+				<img 
+					src={user.profilePic} 
+					alt={user.nickname} 
+				/>
 			</div>
-			<i 
-				className="fa-solid fa-circle" 
-				style={{color: props.user.connected ? 'green' : 'red' }
-				}>
-			</i>
+			{
+				displayCo &&
+				<i 
+					className="fa-solid fa-circle" 
+					style={{color: user.connected ? 'green' : 'red' }
+					}>
+				</i>
+			}
 		</div>
 	)
 }
