85db26542ee37a25d64c28b6eb63ad790c02810b
owalsh <owalsh@student.42.fr>
2023-05-04 18:58:51 +0200

    :construction: Add upload image in file
    *

--- backend/app/src/users/users.module.ts
+++ backend/app/src/users/users.module.ts
@@ -3,11 +3,23 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { FileUploadMiddleware } from './file-upload.middleware';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [PrismaModule],
+  imports: [PrismaModule,
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+          callback(null, file.originalname);
+        },
+      }),
+    })
+  ],
   exports: [UsersService],
 })
 export class UsersModule {
