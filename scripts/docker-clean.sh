30240ed1ceb356f2b31213de9803c6637ede147c
Yangchi <chenade0312@gmail.com>
2023-05-06 04:09:05 +0200

    :tada: Docker is working for three container, react.js, nest.js, database

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -4,3 +4,6 @@ docker container rm -f $(docker container ls -qa)
 docker volume rm -f $(docker volume ls -q)
 docker image rm -f $(docker image ls -qa)
 docker system prune -af
+
+rm -rf ./data/reactapp/*
+rm -rf ./data/nestapp/*
