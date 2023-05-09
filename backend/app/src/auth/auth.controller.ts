f66e2d53776f58cb0a456d8825b9d38e0970190c
Chenade <chenade0312@gmail.com>
2023-05-09 16:31:07 +0200

    use 42 oauth

--- backend/app/src/auth/auth.controller.ts
+++ backend/app/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthEntity } from './entity/auth.entity';
 import { LoginDto } from './dto/login.dto';
+import { OAuth42Strategy } from './strategy/passport.strategy';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -12,6 +14,13 @@ export class AuthController {
 	@Post('login')
 	@ApiOkResponse({ type: AuthEntity })
 	login(@Body() { name, password }: LoginDto) {
-	return this.authService.login(name, password);
+		return this.authService.login(name, password);
+	}
+	
+	@Get('forty-two')
+	@UseGuards(AuthGuard(), OAuth42Strategy)
+	@ApiOkResponse({ type: AuthEntity })
+	fortyTwo() {
+		return "";
 	}
 }
