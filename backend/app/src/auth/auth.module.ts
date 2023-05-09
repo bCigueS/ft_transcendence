243414624a77f05f53bec134cb1b80ae3eed6cda
Chenade <chenade0312@gmail.com>
2023-05-09 16:31:07 +0200

    use 42 oauth

--- backend/app/src/auth/auth.module.ts
+++ backend/app/src/auth/auth.module.ts
@@ -6,20 +6,24 @@ import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { HttpModule } from '@nestjs/axios';
+import { OAuth42Strategy } from './strategy/passport.strategy'; // import OAuth42Strategy
+import { UseGuards } from '@nestjs/common';
 
 export const jwtSecret = 'zjP9h6ZI5LoSKCRj';
 
 @Module({
   imports: [
     PrismaModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'oauth2' }),
+    HttpModule,
     JwtModule.register({
       secret: jwtSecret,
       signOptions: { expiresIn: '5m' },
     }),
-	UsersModule,
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, OAuth42Strategy], // add OAuth42Strategy here
 })
 export class AuthModule {}
