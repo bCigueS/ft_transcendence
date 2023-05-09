d144eb67f323b33de854e798f3acac30f98df963
owalsh <owalsh@student.42.fr>
2023-05-09 17:39:54 +0200

    :construction: Add CRUD for game entity
    *

--- backend/app/src/games/dto/create-game.dto.ts
+++ backend/app/src/games/dto/create-game.dto.ts
@@ -1,1 +1,19 @@
-export class CreateGameDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+import { GameType, GameState } from '@prisma/client';
+
+export class CreateGameDto {
+   
+	@IsNotEmpty()
+    @ApiProperty()
+    type: GameType;
+
+	@IsNotEmpty()
+    @ApiProperty()
+    level: number;
+
+	@IsNotEmpty()
+    @ApiProperty()
+    state: GameState;
+
+}
