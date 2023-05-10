a9c96cecf3ba6901cc00488c98a9af4aa9309c93
foctavia <fo.fanyoctavia@gmail.com>
2023-05-10 11:58:10 +0200

    :hammer: Fix conflict from merging with main
    *

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-=======
 // import reportWebVitals from './reportWebVitals';
->>>>>>> main
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
