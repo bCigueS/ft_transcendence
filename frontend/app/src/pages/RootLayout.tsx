7c6b0a7e9f21fe6b9e321a60110a339143438732
owalsh <owalsh@student.42.fr>
2023-05-11 15:54:55 +0200

    :heavy_plus_sign: Add merged project back and front
    *

---
+++ frontend/app/src/pages/RootLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Nav from '../components/Nav/Nav';
+
+export default function RootLayout() {
+	return (
+		<>
+			<Nav />
+			<main>
+				<Outlet/>
+			</main>
+		</>
+	)
+}
