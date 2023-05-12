155636a1093b8183cc677cfecfe45f13b4d9aca7
owalsh <owalsh@student.42.fr>
2023-05-12 15:20:46 +0200

    :heavy_plus_sign: Add Leaderbord fetching data from the back
    *

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -7,6 +7,10 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    // origin: 'http://localhost:3000'
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
