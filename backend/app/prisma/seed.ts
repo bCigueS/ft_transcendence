cc27e3594a496c70a57b794af7ebd6829a894370
sbeylot <sbeylot@student.42.fr>
2023-05-15 12:25:26 +0200

    :construction: Modify leaderbord and user menu in front
    *

---
+++ backend/app/prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  const simon = await prisma.user.upsert({
+    where: { name: 'Simon' },
+    update: {},
+    create: {
+      email: 'sbeylot@student.42.fr',
+      name: 'Simon',
+      password: 'lolilolilol',
+      avatar: 'sbeylot.jpg'
+    },
+  })
+  const fany = await prisma.user.upsert({
+    where: { name: 'Faaaany' },
+    update: {},
+    create: {
+      email: 'foctavia@student.42.fr',
+      name: 'Faaaany',
+      password: 'lolilolilol',
+      avatar: 'foctavia.jpg'
+    },
+  })
+  const olivia = await prisma.user.upsert({
+    where: { name: 'Olivia' },
+    update: {},
+    create: {
+      email: 'owalsh@student.42.fr',
+      name: 'Olivia',
+      password: 'lolilolilol',
+      avatar: 'owalsh.jpg'
+    },
+  })
+  const yangchi = await prisma.user.upsert({
+    where: { name: 'Yang Chi' },
+    update: {},
+    create: {
+      email: 'ykuo@student.42.fr',
+      name: 'Yang Chi',
+      password: 'lolilolilol',
+      avatar: 'ykuo.jpg'
+    },
+  })
+  console.log({ simon, fany, olivia, yangchi })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
