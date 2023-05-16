37dbd04722049bcbe6130b0fbf462d118a0706cf
sbeylot <sbeylot@student.42.fr>
2023-05-16 13:40:14 +0200

    :construction: Building Login
    *

--- frontend/app/src/components/Nav/ProfileMenu.tsx
+++ frontend/app/src/components/Nav/ProfileMenu.tsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import { UserContext } from "../../store/users-contexte";
+// import { UserContext } from "../../store/users-contexte";
 
 interface menuOption {
 	icon1: string,
