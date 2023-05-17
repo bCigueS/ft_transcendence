dc45cba1ca275dde694060fb266babaf2b95326b
sbeylot <sbeylot@student.42.fr>
2023-05-17 10:58:20 +0200

    :construction: Fetching Users list for leaderboard, fetching Friends
    *

--- frontend/app/src/components/Profile/ProfileCard.tsx
+++ frontend/app/src/components/Profile/ProfileCard.tsx
@@ -1,39 +1,28 @@
 import React, { useContext } from 'react';
 
-import { User, UserContext } from '../../store/users-contexte';
+import { UserAPI } from '../../store/users-contexte';
 
 import classes from '../../sass/components/Profile/ProfileCardInfo.module.scss';
 import ProfilIcon from './ProfilIcon';
 
-const ProfileCard: React.FC<{ user?: User}> = ( { user } ) => {
-
-	const userCtx = useContext(UserContext);
+const ProfileCard: React.FC<{ user?: UserAPI | null}> = ( { user } ) => {
 
 	return (
 		<div className={classes.container}>
 			<ProfilIcon user={user} displayCo={false} size={['15rem', '15rem']}/>
 			<div className={classes.user}>
-				<h1>{user?.nickname}</h1>
-				{/* <p>{user?.email}</p> */}
+				<h1>{user?.name}</h1>
+				<p>{user?.email}</p>
 			</div>
 			<div className={classes.stat}>
 				<div className={classes.info}>
 					<i className="fa-solid fa-trophy"></i>
 					<p>{user?.wins}</p>
 				</div>
-				<div className={classes.info}>
-					<i className="fa-solid fa-bolt"></i>
-					<p>{user?.lose}</p>
-				</div>
+
 				<div className={classes.info}>
 					<i className="fa-sharp fa-solid fa-chart-simple"></i>
-					<p>
-						{ user &&
-							user.lose !== 0
-							? `${user.wins / (user.wins + userCtx.user.lose) * 100}%`
-							: '100' 
-						}
-					</p>
+					<p>{ user?.gamesPlayed }</p>
 				</div>
 			</div>
 		</div>
