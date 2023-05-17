17d97d20e72245681b651c9a19c1b3b6665e98ec
sbeylot <sbeylot@student.42.fr>
2023-05-17 16:26:58 +0200

    :construction: Building Fetching User
    *

--- frontend/app/src/components/Profile/ProfileFriends.tsx
+++ frontend/app/src/components/Profile/ProfileFriends.tsx
@@ -1,24 +1,24 @@
 import React, { useContext } from 'react';
 
-import { User, UserContext } from '../../store/users-contexte';
+import { User, UserAPI, UserContext } from '../../store/users-contexte';
 import classes from '../../sass/components/Profile/ProfileFriends.module.scss';
 import ProfilIcon from './ProfilIcon';
 
-const ProfileFriends: React.FC<{user: User; block: boolean; friend: boolean}> = ( props ) => {
+const ProfileFriends: React.FC<{user: UserAPI | null; block: boolean; friend: boolean}> = ( props ) => {
 
 	const userCtx = useContext(UserContext);
 
-	const addBlockHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
-		userCtx.blockUser(props.user);
-	};
+	// const addBlockHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
+	// 	userCtx.blockUser(props.user);
+	// };
 
-	const removeBlockHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
-		userCtx.unblockUser(props.user);
-	};
+	// const removeBlockHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
+	// 	userCtx.unblockUser(props.user);
+	// };
 
-	const removeFriendHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
-		userCtx.unfriendUser(props.user);
-	}
+	// const removeFriendHandler = (event: React.MouseEvent<HTMLIFrameElement, MouseEvent>) => {
+	// 	userCtx.unfriendUser(props.user);
+	// }
 
 	return (
 		<div className={classes.container}>
@@ -26,7 +26,7 @@ const ProfileFriends: React.FC<{user: User; block: boolean; friend: boolean}> = 
 			<ProfilIcon user={props.user} />
 
 			<div className={classes.info}>
-				<h1>{props.user.nickname}</h1>
+				<h1>{props.user?.name}</h1>
 				<i 
 					title='Private Message'
 					className='fa-solid fa-message'>
@@ -37,12 +37,12 @@ const ProfileFriends: React.FC<{user: User; block: boolean; friend: boolean}> = 
 			<div className={classes.option}>
 				<i 
 					title={props.block ? "Unblock" : "Block"}
-					onClick={props.block ? removeBlockHandler : addBlockHandler}
+					// onClick={props.block ? removeBlockHandler : addBlockHandler}
 					className={props.block ? 'fa-solid fa-lock' : 'fa-solid fa-lock-open'}>
 				</i>
 				<i 
 					title='Friend'
-					onClick={removeFriendHandler}
+					// onClick={removeFriendHandler}
 					className={props.friend ? 'fa-solid fa-user-minus' : 'fa-solid fa-user-plus'}>
 				</i>
 			</div>
