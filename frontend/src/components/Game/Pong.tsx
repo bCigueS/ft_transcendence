13aa572d708b28f852d499e5ffb800c34801337c
Fany Octavia <foctavia@student.42.fr>
2023-05-23 13:23:41 +0200

    :memo: Add comments and informations on each function in the game
    *

--- frontend/src/components/Game/Pong.tsx
+++ frontend/src/components/Game/Pong.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
+import { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import ModalBoard from './ModalBoard';
 import LiveBoard from './LiveBoard';
 import classes from '../../sass/components/Game/Pong.module.scss';
 import io from 'socket.io-client';
-import { Sign } from 'crypto';
 
 const socket = io('http://localhost:3001');
 
@@ -41,6 +40,7 @@ type PongProps = {
 	username: string;
 }
 
+// initial data for the game
 const info: PongInfo = {
 	boardWidth: 640,
 	boardHeight: 480,
@@ -49,7 +49,7 @@ const info: PongInfo = {
 	initialDelta: 3,
 	playerX: 10,
 	opponentX: 620, // boardWidth - paddleWidth - 10,
-	winnerScore: 3,
+	winnerScore: 11,
 }
 
 
@@ -92,10 +92,11 @@ export default function Pong({username}: PongProps) {
 
 	useEffect(() => {
 		if (playerMode === DOUBLE_MODE) {
-
+			// if the game is for 2 players mode, start by sending a join request to the server
 			socket.emit('join', { name: username, level: level }, (message: string) => {
 				console.log(message);
 			});
+			// receive a welcome message from server informing that you are in a specific game room, and trigger a liveBoard
 			socket.on('welcome', ({ message, opponent, gameId }) => {
 				console.log({ message, opponent, gameId });
 				setGameId(gameId);
@@ -105,17 +106,21 @@ export default function Pong({username}: PongProps) {
 					setOpponentName(opponent.name);
 				}
 			});
+			// receive an information about the opponent from server
 			socket.on('opponentJoin', ({ message, opponent }) => {
 				console.log({ message, opponent });
 				setOpponentName(opponent.name);
 			});
+			// receive a confirmation from server to start the game, and trigger the liveBoard to start counting the countdown
 			socket.on('startGame', ({ message }) => {
 				console.log({ message });
 				setIsReady(true);
 			});
+			// receive a message from a server that an opponent just left the game
 			socket.on('opponentLeft', ({message}) => {
 				console.log({message});
 			});
+			// receive a message from server to stop the game, and trigger the modalBoard
 			socket.on('stopGame', ({ message }) => {
 				console.log({ message });
 				setGameOver(true);
@@ -125,9 +130,10 @@ export default function Pong({username}: PongProps) {
 		}
 	}, [playerMode])
 	
+	// function to detect when a ball hit the paddle of the opponent side
 	const detectOpponentCollision = () => {
 		if (ballX + ballRadius >= info.opponentX && ballY > opponentY && ballY < opponentY + paddleHeight) {
-
+			// if the game is against computer, the calculation for the new direction is directly in the front
 			if (playerMode === SINGLE_MODE) {
 				setDeltaX(x => x *= -1);
 				
@@ -138,7 +144,9 @@ export default function Pong({username}: PongProps) {
 				
 				setDeltaX(-speed * Math.cos(angle));
 				setDeltaY(speed * Math.sin(angle));
+			// if the game is against other player, calculation will be done by server
 			} else if (playerMode === DOUBLE_MODE) {
+				// receiving the new ball direction from server
 				socket.on('ballLaunch', ({dx, dy}) => {
 					// console.log("opponent dx, dy: ", dx, dy);
 					setDeltaX(dx);
@@ -153,9 +161,10 @@ export default function Pong({username}: PongProps) {
 		}
 	}
 
+	// function to detect when a ball hit the paddle of the player side
 	const detectPlayerCollision = () => {
 		if (ballX - ballRadius <= info.playerX + info.paddleWidth && ballY > playerY && ballY < playerY + paddleHeight) {
-			
+			// send a signal to server to start calculating a new direction of the ball
 			socket.emit('ballCollision', {
 				gameInfo: {
 					y: ballY,
@@ -166,8 +175,8 @@ export default function Pong({username}: PongProps) {
 					speed: speed,
 				}, gameId: gameId,
 			});
+			// if the game is against computer, the calculation for the new direction is directly in the front
 			if (playerMode === SINGLE_MODE) {
-				
 				let collisionPoint = (ballY + (ballRadius / 2)) - (playerY + (paddleHeight / 2));
 				collisionPoint = collisionPoint / (paddleHeight / 2);
 				
@@ -175,7 +184,9 @@ export default function Pong({username}: PongProps) {
 				
 				setDeltaX(speed * Math.cos(angle));
 				setDeltaY(speed * Math.sin(angle));
+			// if the game is against other player, calculation will be done by server
 			} else if (playerMode === DOUBLE_MODE) {
+				// receiving the new ball direction from server
 				socket.on('ballLaunch', ({dx, dy}) => {
 					// console.log("player dx, dy: ", dx, dy);
 					setDeltaX(dx);
@@ -190,6 +201,7 @@ export default function Pong({username}: PongProps) {
 		}
 	}
 	
+	// function to set an initial ball position and direction to start the round
 	const serve = (side: number) => {
 
 		setBallX(info.boardWidth / 2);
@@ -197,22 +209,23 @@ export default function Pong({username}: PongProps) {
 		
 		setSpeed(info.initialSpeed + level);
 		
+		// if the game is against computer, the calculation for the ball direction is directly in the front
 		if (playerMode === SINGLE_MODE) {
-
 			setDeltaX((info.initialDelta + level) * side);
 			setDeltaY(5 * (Math.random() * 2 - 1));
-
+		// if the game is against other player, calculation will be done by server
 		} else if (playerMode === DOUBLE_MODE) {
+			// receiving the ball direction from server
 			socket.on('ballServe', ({dx, dy}) => {
 				// console.log("serve ball dx, dy: ", dx, dy);
 				setDeltaX(dx);
 				setDeltaY(dy);
 			});
 		}
-
 		// console.log(ballX, ballY, deltaX, deltaY, speed);
 	}
 	
+	// function to set initial value to start the game
 	const startGame = (side: number) => {
 		if (!isRunning) {
 			switch (level) {
@@ -236,6 +249,7 @@ export default function Pong({username}: PongProps) {
 			setGameOver(false);
 		}
 		
+		// send a signal to server to start a calculation of the ball direction to start the round
 		socket.emit('startBall', {
 			gameInfo: {
 				initialDelta: info.initialDelta,
@@ -243,10 +257,13 @@ export default function Pong({username}: PongProps) {
 			}, gameId: gameId,
 		});
 		
+		// call serve function to set the ball values
 		serve(side);
+		// toggle isRunning boolean to start the animation of the game
 		setIsRunning(true);
 	}
 	
+	// function to detect ball collision with all 4 part of the walls/borders
 	const detectWallCollision = () => {
 		const minY    = ballRadius;
 		const maxY    = info.boardHeight - ballRadius;
@@ -261,9 +278,10 @@ export default function Pong({username}: PongProps) {
 			setDeltaY(y => y * -1);
 			setBallY(maxY);
 		}
-		// left collision
+		// left collision / ball passing the player's paddle, so opponent gains a point
 		if (ballX <= 0)
 		{
+			// send signal to server to calculate the ball direction for a new round
 			socket.emit('startBall', {
 				gameInfo: {
 					initialDelta: info.initialDelta,
@@ -274,7 +292,7 @@ export default function Pong({username}: PongProps) {
 			setOpponentScore(o => o += 1);
 			serve(OPPONENT_SIDE);
 		}
-		//right collision
+		//right collision / ball passing the opponent's paddle, so player gains a point
 		if (ballX >= info.boardWidth)
 		{
 			setPlayerScore(p => p += 1);
@@ -282,12 +300,15 @@ export default function Pong({username}: PongProps) {
 		}
 	}
 	
+	// function to calculate the movement of the ball based on its direction
 	const moveBall = () => {
 		setBallX(x => x += deltaX);
 		setBallY(y => y += deltaY);
 	}
 
+	// function to calculate the opponent movement (against computer or other player)
 	const moveOpponent = () => {
+		// calculating movement of the computer
 		const nextPos = ballY - (paddleHeight / 2) * (level === HARD_LEVEL ? 0.3 : 0.1);
 		
 		if (playerMode === SINGLE_MODE 
@@ -295,12 +316,14 @@ export default function Pong({username}: PongProps) {
 			&& nextPos + paddleHeight <= info.boardHeight) {
 			setOpponentY(nextPos);
 		} else if (playerMode === DOUBLE_MODE) {
+			// receiving a new position from the server, based on the other player's input
 			socket.on('paddleMove', ({y}) => {
 				setOpponentY(y);
 			});
 		}
 	}
 
+	// function to calculate the players movement based on its input (mouse event or keyboard event)
 	const movePlayer = () => {
 		if (toolMode === KEYBOARD_MODE && isRunning)
 		{
@@ -309,18 +332,21 @@ export default function Pong({username}: PongProps) {
 			if (paddleUp && nextPostUp >= 0) {
 				setPlayerY(nextPostUp);
 				if (playerMode === DOUBLE_MODE) {
+					// send the new position to server to be forwarded to other player
 					socket.emit('moveInput', {y: nextPostUp, gameId: gameId});
 				}
 			}
 			if (paddleDown && nextPostDown <= info.boardHeight) {
 				setPlayerY(nextPostDown - paddleHeight);
 				if (playerMode === DOUBLE_MODE) {
+					// send the new position to server to be forwarded to other player
 					socket.emit('moveInput', {y: nextPostDown - paddleHeight, gameId: gameId});
 				}
 			}
 		}
 	}
 	
+	// function to draw the board with its initial element
 	const drawBoard = (context: CanvasRenderingContext2D) => {
 		context.clearRect(0, 0, info.boardWidth, info.boardHeight);
 		// draw background
@@ -334,6 +360,7 @@ export default function Pong({username}: PongProps) {
 		context.fillText(' ' + opponentScore, 345, 50);
 	}
 	
+	// function to draw the elements of the game
 	const drawElement = (context: CanvasRenderingContext2D) => {
 		// draw player
 		context.fillStyle = '#F2F2F2';
@@ -353,6 +380,7 @@ export default function Pong({username}: PongProps) {
 		context.stroke();
 	}
 
+	// function to stop the animation by toggling the isRunning bool, and send a leave request to the server after 1 second
 	const stopGame = () => {
 		setIsRunning(false);
 		if (playerMode === DOUBLE_MODE) {
@@ -364,7 +392,7 @@ export default function Pong({username}: PongProps) {
 		}
 	}
 
-	// render game
+	// render the game
 	useLayoutEffect(() => {
 		const canvas = canvasRef.current;
 		if (!canvas)
@@ -374,6 +402,7 @@ export default function Pong({username}: PongProps) {
 		return;
 		
 		drawBoard(context);
+		// start animation
 		if (isRunning) {
 			drawElement(context);
 			moveBall();
@@ -389,12 +418,12 @@ export default function Pong({username}: PongProps) {
 				stopGame();
 			}
 		}
-
 	}, [frameCount])
 	
 	// update the frameCount
 	useLayoutEffect(() => {
 		if (!isPaused) {
+			// set frame per second
 			const fps = 24;
 			let frameId: number;
 	
@@ -441,7 +470,7 @@ export default function Pong({username}: PongProps) {
 			}
 		}
 
-	}, [frameCount]);
+	}, [toolMode]);
 	
 	// mouse event handler
 	useEffect(() => {
@@ -467,7 +496,7 @@ export default function Pong({username}: PongProps) {
 				}
 			}
 		}
-	}, [frameCount])
+	}, [toolMode])
 
 	return (
 		<>
--- pong-server/src/index.ts
+++ pong-server/src/index.ts
@@ -7,12 +7,14 @@ const { Server, Socket } = require('socket.io');
 const express = require('express');
 const cors = require('cors');
 
+// port is 3001 because port 3000 is already used
 const PORT = 3001;
 const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
 
+// creating a new server that connect to the origin address of localhost:3000
 const io = new Server(server, {
 	cors: {
 		origin: 'http://localhost:3000',
@@ -20,30 +22,35 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket: typeof Socket) => {
-
+	// receiving a join request with name and level as a parameter
 	socket.on('join', ({ name, level }: { name: string, level: number }, callback: CallbackInfo) => {
+		// creating a player with addPlayer function that put this new player inside a games array
 		const {player, opponent, message} = addPlayer({
 			name,
 			playerId: socket.id,
 			level,
 			gameId: 'pong',
 		});
+
+		// joining a game room that the room id is the gameId of the player
 		socket.join(player.gameId);
+		// send a callback message informing the successfull join process
 		callback(message);
 
-		// send welcome message to player1, and also send the opponent player's data
+		// send welcome message to player, and also send the opponent player's data (if any)
 		socket.emit('welcome', {
 			message: `Hello ${player.name}, Welcome to the game`,
 			opponent,
 			gameId: player.gameId,
 		});
 		
-		// tell player1 that player2 has joined the game
+		// tell first player that second player has joined the game
 		socket.broadcast.to(player.gameId).emit('opponentJoin', {
 			message: `${player.name} has joined the game`,
 			opponent: player,
 		});
 		
+		// when the room already has 2 players, send a message to both players that they can start the game
 		if (game(player.gameId).length >= 2) {
 			io.to(player.gameId).emit('startGame', {
 				message: `Let's start the game!`,
@@ -51,9 +58,12 @@ io.on('connection', (socket: typeof Socket) => {
 		}
 	});
 	
+	// receiving a startBall request to inform the server to start a calculation for the direction of the ball to the players
 	socket.on('startBall', ({gameInfo, gameId}: {gameInfo: ServeInfo, gameId: string}) => {
+		// creating direction of the ball for the start of the game with serveBall function
 		const {dx, dy} = serveBall(gameInfo);
 
+		// get the players in the room and send the ball direction to both players (horizontal direction is in reverse/mirror)
 		let room = io.sockets.adapter.rooms.get(gameId);
 		if (room) {
 			for (let id = room.values(), val = null; val = id.next().value; ) {
@@ -65,9 +75,12 @@ io.on('connection', (socket: typeof Socket) => {
 		}
 	});
 	
+	// receiving a ballCollision request to inform the server that the ball hit a paddle of the player and need a new ball direction
 	socket.on('ballCollision', ({gameInfo, gameId}: {gameInfo: CollisionInfo, gameId: string}) => {
+		// creating a new direction of the ball after it hit a player paddle with ballCollision function
 		let {dx, dy} = ballCollision(gameInfo);
 
+		// get the players in the room and send the new ball direction to both players (horizontal direction is in reverse/mirror)
 		let room = io.sockets.adapter.rooms.get(gameId);
 		if (room) {
 			for (let id = room.values(), val = null; val = id.next().value; ) {
@@ -79,31 +92,41 @@ io.on('connection', (socket: typeof Socket) => {
 		}
 	})
 		
+	// receiving a new position of the player paddle and send it to the other player (to sync the movement as an opponent movement)
 	socket.on('moveInput', ({ y, gameId }: {y: number, gameId: string}) => {
 		socket.broadcast.to(gameId).emit('paddleMove', { y });
 	});
 
+	// receiving a leave request, so that the player can be removed from the games array and room
 	socket.on('leave', (gameId: string, callback: CallbackInfo) => {
+		// removing a player with removePlayer function and return the address of the deleted player
 		const {player, message} = removePlayer(socket.id, gameId);
 
 		if (player) {
+			// send message to the other player in the room (if any) that a player just left
 			socket.broadcast.to(gameId).emit('opponentLeft', {
 				message: `${player.name} has left the game`,
 			});
 
+			// leave the game room
 			socket.leave(gameId);
+			// send a callback message informing the successfull process
 			callback(message);
 			// console.log('length of game: ' + game(player.gameId).length);
 		}
 	});
 
+	// receiving a disconnect request, when a connection of a player disrupted
 	socket.on('disconnect', () => {
+		// removing a player with removePlayer function and return the address of the deleted player
 		const {player, message} = removePlayer(socket.id);
 
 		if (player) {
+			// send message to the other player in the room (if any) that a player just left
 			socket.broadcast.to(player.gameId).emit('opponentLeft', {
 				message: `${player.name} has left the game`,
 			});
+			// send an additional message to all player in the room to stop the game and make the player that still in the room as a winner of the recent game
 			io.to(player.gameId).emit('stopGame', {
 				message: `Game has ended, you won!`,
 			});
