8987959a4c0c57c73ab7da8ed0af92f034c40374
sbeylot <sbeylot@student.42.fr>
2023-05-24 09:37:04 +0200

    Revert "Merge branch 'fany/frontend/socket' into Frontend/Simon"
    This reverts commit 83493b80501c2b199bd476684d3768afa2481fd4, reversing
    changes made to f9b9d582839a2762f49715bd7e5ab0aca27fb3f7.

--- frontend/app/src/pages/Game.tsx
+++ frontend/app/src/pages/Game.tsx
@@ -1,18 +1,5 @@
-import { useState } from 'react';
-import Pong from '../components/Game/Pong';
-
-import classes from '../sass/pages/Game.module.scss';
-
 export default function Game() {
-	// player info
-	const [username, setUserName] = useState('Fany');
-	const [gameId, setGameId] = useState('19');
-
 	return (
-		<div className={classes.gamePage}>
-			<Pong 
-				username={username}
-			/>
-		</div>
+		<h1>Game Pong Page</h1>
 	)
 }
