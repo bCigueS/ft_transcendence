2099f61224f3ba093e2e6966df690a8868baf530
Fany Octavia <foctavia@student.42.fr>
2023-05-25 11:28:30 +0200

    :heavy_plus_sign: Working pong server inside nestjs app
    *

--- backend/app/src/socket-io-adapter.ts
+++ backend/app/src/socket-io-adapter.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { Server, ServerOptions } from 'socket.io';
+import * as redisAdapter from '@socket.io/redis-adapter';
+
 
 export class SocketIOAdapter extends IoAdapter {
   private readonly logger = new Logger(SocketIOAdapter.name);
@@ -28,6 +30,17 @@ export class SocketIOAdapter extends IoAdapter {
     };
 
     const server: Server = super.createIOServer(port, optionsWithCORS);
+
+
+    // // /Add Redis adapter
+    // const redisOptions = {
+	// 	host: 'localhost',
+	// 	port: 6379,
+	//   };
+  
+	//   const redisAdapterInstance = redisAdapter(redisOptions);
+	//   server.adapter(redisAdapterInstance);
+  
 
     return server;
   }
