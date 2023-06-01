659b77b00ab887af2e9a46fe49bd7c28ba5edebc
sbeylot <sbeylot@student.42.fr>
2023-06-01 12:40:22 +0200

    :art: Changing graphism
    *

--- frontend/app/src/components/Auth/AuthForm.tsx
+++ frontend/app/src/components/Auth/AuthForm.tsx
@@ -4,6 +4,8 @@ import { setTokenAuth } from '../../typescript/Auth';
 import { UserContext } from '../../store/users-contexte';
 
 import classes from '../../sass/components/Auth/AuthForm.module.scss';
+import ProfilPatch from '../Profile/ProfilPatch';
+import DoubleAuthPannel from './DoubleAuthPannel';
 
 
 /* 
@@ -57,29 +59,42 @@ const AuthForm = () => {
 			{	!isLogged &&
 				<div className={classes.loggin}>
 					<a  href={`http://127.0.0.1:3000/auth/forty-two`}>
-					<button className = {classes.button}>Log in with 42</button>
+					<button 
+						className = {classes.button}>
+							Log in with<br/>
+							<span>42</span>
+					</button>
 					</a>
 				</div>
 			}
 
 			{
 				isLogged &&
-				<Form method='patch' className={classes.container}>
-					<h1>User Information</h1>
-					<div className={classes.label}>
-						<label htmlFor="name">User Name</label>
-						<input type="text" id="name" name='name' />
-					</div>
-					<div className={classes.label}>
-						<label htmlFor="switch">Double Authentication</label>
-						<input type="checkbox" id="switch" name='auth' />
-					</div>
-					<div className={classes.label}>
-						<label htmlFor="avatar">Avatar</label>
-						<input type="file" id="avatar" name='avatar' />
-					</div>
-					<button>Confirm</button>
-				</Form>
+				<ProfilPatch/>
+				// <DoubleAuthPan	// .tab {
+	// 	overflow: hidden;
+	// 	display: flex;
+	// 	justify-content: center;
+	// 	flex-direction: row;
+	// 	gap: 2rem;
+		
+	// 	.btn {
+	// 		background-color: inherit;
+	// 		float: left;
+	// 		border: none;
+	// 		outline: none;
+	// 		cursor: pointer;
+	// 		padding: 1rem 1.4rem;
+	// 	}
+	// 	.active {
+	// 		@include shadowBox();
+	// 		padding: 0.8rem 1.2rem;
+
+	// 		border: 0.2rem solid black;
+	// 		border-bottom: none;
+	// 		background-color: $almost-white;
+	// 	}
+	// }nel />
 			}
 		</>
 	)
