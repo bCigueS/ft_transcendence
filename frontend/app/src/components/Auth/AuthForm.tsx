3c5fcaf5a5be8576538e2f400200d5c94f47d002
sbeylot <sbeylot@student.42.fr>
2023-06-02 15:08:29 +0200

    :construction: Implement basic authentication need to refactor
    *

--- frontend/app/src/components/Auth/AuthForm.tsx
+++ frontend/app/src/components/Auth/AuthForm.tsx
@@ -1,62 +1,76 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
-import { Form, redirect, useSearchParams } from 'react-router-dom';
-import { setTokenAuth } from '../../typescript/Auth';
+import React, { useContext, useState, FormEvent } from 'react';
+import { Form } from 'react-router-dom';
 import { UserContext } from '../../store/users-contexte';
 
 import classes from '../../sass/components/Auth/AuthForm.module.scss';
 import ProfilPatch from '../Profile/ProfilPatch';
 import DoubleAuthPannel from './DoubleAuthPannel';
 
-
-/* 
-*	Component for User connection throught 42 API
-*/
+interface AuthFormProps {
+	onAuthenticate: (formData: FormData) => Promise<void | Response>;
+}
 
 
 //	PREVIOUS VERSION
 
-const AuthForm = () => {
+const AuthForm: React.FC<AuthFormProps> = ({ onAuthenticate }) => {
 
-	const userCtx = useContext(UserContext);
-	const [ isLogged, setIsLogged ] = useState<boolean>(false);
-	const [ logCode, setLogCode ] = useState<string>("");
-	const [ token, setToken ] = useState<string>("");
-	
-	useEffect(() => {
-		if (window.location.href.includes("code="))
-			setLogCode(window.location.href.split("code=")[1])
-	}, [])
+	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const formData = new FormData(event.currentTarget);
+		await onAuthenticate(formData);
+	}	
+	// useEffect(() => {
+	// 	if (window.location.href.includes("code="))
+	// 		setLogCode(window.location.href.split("code=")[1])
+	// }, [])
 
-	useEffect(() => {
-		if (logCode !== "") {
-			const fetchToken = async () => {
-				const response = await fetch("http://localhost:3000/auth/me", {
-					method: "POST",
-					headers: {
-					"Content-Type": "application/json"
-					},
-					body: JSON.stringify({code: logCode}),
-				});
-				if (response.ok) {
-					const data = await response.json();
-					setToken(data.token.access_token);
-				}
-			}
-			fetchToken()
-		}
-	}, [logCode])
+	// useEffect(() => {
+	// 	if (logCode !== "") {
+	// 		const fetchToken = async () => {
+	// 			const response = await fetch("http://localhost:3000/auth/me", {
+	// 				method: "POST",
+	// 				headers: {
+	// 				"Content-Type": "application/json"
+	// 				},
+	// 				body: JSON.stringify({code: logCode}),
+	// 			});
+	// 			if (response.ok) {
+	// 				const data = await response.json();
+	// 				setToken(data.token.access_token);
+	// 			}
+	// 		}
+	// 		fetchToken()
+	// 	}
+	// }, [logCode])
 
-	useEffect(() => {
-		if (token) {
-			userCtx.saveToken(token);
-			setTokenAuth(token);
-			setIsLogged(true);
-		}
-	}, [token, isLogged])
+	// useEffect(() => {
+	// 	if (token) {
+	// 		userCtx.saveToken(token);
+	// 		setTokenAuth(token);
+	// 		setIsLogged(true);
+	// 	}
+	// }, [token, isLogged])
+
 
 	return (
 		<>
-			{	!isLogged &&
+			<form method='post' onSubmit={handleSubmit}>
+				<h1>Connect Debug</h1>
+				<p>Password is 'lolilolilol'</p>
+				<div>
+					<label htmlFor="name">Username</label>
+					<input type="text" name="name" id="name" />
+				</div>
+
+				<div>
+					<label htmlFor="password">Password</label>
+					<input type="password" name="password" id="password" />
+				</div>
+				<button type="submit">Connect</button>
+			</form>
+						
+			{/* {	!isLogged &&
 				<div className={classes.loggin}>
 					<a  href={`http://127.0.0.1:3000/auth/forty-two`}>
 					<button 
@@ -71,31 +85,8 @@ const AuthForm = () => {
 			{
 				isLogged &&
 				<ProfilPatch/>
-				// <DoubleAuthPan	// .tab {
-	// 	overflow: hidden;
-	// 	display: flex;
-	// 	justify-content: center;
-	// 	flex-direction: row;
-	// 	gap: 2rem;
-		
-	// 	.btn {
-	// 		background-color: inherit;
-	// 		float: left;
-	// 		border: none;
-	// 		outline: none;
-	// 		cursor: pointer;
-	// 		padding: 1rem 1.4rem;
-	// 	}
-	// 	.active {
-	// 		@include shadowBox();
-	// 		padding: 0.8rem 1.2rem;
-
-	// 		border: 0.2rem solid black;
-	// 		border-bottom: none;
-	// 		background-color: $almost-white;
-	// 	}
-	// }nel />
-			}
+				// <DoubleAuthPannel />
+			} */}
 		</>
 	)
 }
