f851b4338b38ea168c8c7149429bd94b55657153
olivwalsh <oliviawalsh073@gmail.com>
2023-06-12 18:18:02 +0200

    :construction: Add send message with sockets

--- backend/app/src/messages/messages.gateway.ts
+++ backend/app/src/messages/messages.gateway.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, NotFoundException, Logger } from '@nestjs/common';
-import { OnGatewayInit, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayInit, WebSocketGateway, OnGatewayConnection, OnGatewayDisconnect, WebSocketServer, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Socket, Server, Namespace } from 'socket.io';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
@@ -9,7 +9,7 @@ import { UpdateMessageDto } from './dto/update-message.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { MessageEntity } from './entities/message.entity';
 
-@WebSocketGateway({ namespace: '/chat' })
+@WebSocketGateway({ namespace: '/chat', cors: '*' })
 export class MessagesGateway implements OnGatewayInit, OnGatewayConnection {
   private readonly logger = new Logger(MessagesGateway.name);
 
@@ -18,16 +18,35 @@ export class MessagesGateway implements OnGatewayInit, OnGatewayConnection {
   @WebSocketServer() io: Namespace;
   @WebSocketServer() server: Server
 
-  // Gateway initialized (provided in module and instantiated)
   afterInit(): void {
     this.logger.log(`Websocket chat gateway initialized.`);
   }
 
+  @SubscribeMessage('message')
+  async handleMessage(client: Socket, message: { 
+				content: string,
+				channelId: number,
+				senderId: number }): Promise<void> {
+
+	console.log('message: ', message);
+
+	const newMessage = await this.prisma.message.create({
+		data: {
+			senderId: message.senderId,
+			content: message.content,
+			channelId: message.channelId
+		}
+	});
+
+	this.server.emit('message', message.content);
+  }
+  
   async handleConnection(client: Socket) {
 	client.on('send', ({ content }: {content: string}) => {
 		console.log('text: ', content);
+
+		this.server.emit('chat', content);
 		
-		// create message and send the channel id
 	})
   }
 
