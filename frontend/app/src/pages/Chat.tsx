1c370a389d0e870ba5d37da2352308337913a647
olivwalsh <oliviawalsh073@gmail.com>
2023-06-12 18:18:02 +0200

    :construction: Add send message with sockets

--- frontend/app/src/pages/Chat.tsx
+++ frontend/app/src/pages/Chat.tsx
@@ -4,42 +4,7 @@ import classes from '../sass/pages/Chat.module.scss';
 import { UserAPI, UserContext } from '../store/users-contexte';
 import Message from '../components/Chat/Message';
 import NoConvo from '../components/Chat/NoConvo';
-import io from 'socket.io-client';
-
-
-// GET http://localhost:3000/channels/userId/1
-// [
-//     {
-//         "id": 1,
-//         "name": "private",
-//         "messages": [
-//             {
-//                 "createdAt": "2023-06-12T09:16:21.758Z",
-//                 "id": 1,
-//                 "senderId": 1,
-//                 "content": "Hey girl",
-//                 "channelId": 1
-//             }
-//         ],
-//         "members": [
-//             {
-//                 "id": 1,
-//                 "channelId": 1,
-//                 "userId": 1
-//             },
-//             {
-//                 "id": 2,
-//                 "channelId": 1,
-//                 "userId": 2
-//             }
-//         ]
-//     }
-// ]
-
-const socket = io('http://localhost:3000/chat', {
-		transports: ["websocket"],
-		}
-	);
+import io, { Socket } from 'socket.io-client';
 
 export interface Message {
 	createdAt: Date,
@@ -58,27 +23,51 @@ export interface Channel {
 
 }
 
-export interface ChatAPI {
-	// sender: UserAPI,
-	id: number,
-	// sender: string;
-	senderId: number;
-	messages?: string[],
-	lastMessage: string,
-	
-}
-
-export interface Message {
-	
-}
-
 export default function Chat() {
 	
 	const [ selectedConversationId, setSelectedConversationId ] = useState<number>(0);
 	const [ selectedConversation, setSelectedConversation ] = useState<Channel | null>();
+	const [ chats, setChats ] = useState<Channel[]>([]);
+	const [ socket, setSocket ] = useState<Socket>();
+	const [ messages, setMessages ] = useState<Message[] >([]);
+
 	const messageInput = useRef<HTMLInputElement>(null);
 	const userCtx = useContext(UserContext);
-	const [chats, setChats] = useState<Channel[]>([]);
+
+	const send = (content: string, selectedConversation: number) => {
+
+		const message = {
+		  content: content,
+		  channelId: selectedConversation,
+		  senderId: userCtx.user?.id
+		};
+	  
+		socket?.emit("message", message);
+
+	}
+
+	useEffect(() => {
+		const newSocket = io("http://localhost:3000/chat");
+		setSocket(newSocket);
+	}, [setSocket])
+
+	const messageListener = (message: string) => {
+		const newMessage = {
+			id: Math.random(),
+			createdAt: new Date(),
+            senderId: 1,
+            content: message,
+            channelId: selectedConversationId,
+        };
+		setMessages([...messages, newMessage]);
+	}
+
+	useEffect(() => {
+		socket?.on("message", messageListener);
+		return () => {
+			socket?.off("message", messageListener);
+		}
+	}, [messageListener])
 
     useEffect(() => {
         fetch('http://localhost:3000/channels/userId/1')
@@ -101,6 +90,8 @@ export default function Chat() {
 
 		let selectedChannel = chats.find(chat => chat.id === channelId);
 		setSelectedConversation(selectedChannel);
+		if (selectedChannel)
+			setMessages(selectedChannel.messages);
 	}
 	
 	const displayConvo = (chatList: Channel[], channelId: number) => {
@@ -132,7 +123,7 @@ export default function Chat() {
 				return ;
 			}
 			
-			socket.emit('send', {content: enteredText});
+			send(enteredText, selectedConversationId);
 
 			console.log('message input: ', enteredText);
 	
@@ -144,15 +135,18 @@ export default function Chat() {
 		return (
 			<div className={classes.message}>
 				{
-					selectedConversation?.messages.map((message) => 
+					messages.map((message) => 
 						<Message key={message.id}
 								isMine={isMessageMine(message)}
-								isLast={isMessageLast(message, selectedConversation?.messages)} 
+								isLast={isMessageLast(message, messages)} 
 								message={message.content}/>)
 				}
 
 				<form onSubmit={handleSubmit}>
-					<input className={classes.sendInput} type="text" ref={messageInput} placeholder='type here...' />
+					<input className={classes.sendInput} 
+							type="text"
+							ref={messageInput} 
+							placeholder='type here...' />
 				</form>
 			</div>
 		);
