1e1da37f7d4b768cb6b15c75e2325a6e64c4eece
sbeylot <sbeylot@student.42.fr>
2023-06-15 11:52:17 +0200

    :construction: LeaderBoard with hater and all
    *

--- frontend/app/src/pages/Leaderboard.tsx
+++ frontend/app/src/pages/Leaderboard.tsx
@@ -1,5 +1,5 @@
 // Basic Inports
-import { useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { json, useLoaderData } from 'react-router-dom';
 
 //	Components Inports
@@ -8,7 +8,7 @@ import Searchbar from '../components/Leaderboard/Searchbar';
 import FilterSearch from '../components/Leaderboard/FilterSearch';
 
 //	Context Inport
-import { UserAPI } from '../store/users-contexte';
+import { UserAPI, UserContext } from '../store/users-contexte';
 
 //	Functions Inport
 import * as sortU from '../typescript/sortUsers';
@@ -19,9 +19,10 @@ import classes from '../sass/pages/Leaderboard.module.scss';
 
 export default function Leaderboard() {
 
+	const userCtx = useContext(UserContext);
 	const [searchInput, setSearchInput] = useState('');
 	const [filterOption, setFilterOption] = useState('');
-	const userData = useLoaderData() as UserAPI[];
+	const [userCommunity, setUserCommunity] = useState<UserAPI[]>([]);
 
 	const saveSearchInput = (enteredSearchInput: string) => {
 		setSearchInput(enteredSearchInput);
@@ -30,29 +31,54 @@ export default function Leaderboard() {
 	const saveFilterOption = (enteredFilterOption: string) => {
 		setFilterOption(enteredFilterOption);
 	};
+	const fetchCommunity = useCallback(async() => {
+		try {
+			const response = await fetch('http://localhost:3000/users/' + userCtx.user?.id + '/show-community');
+			
+			if (!response.ok)
+			throw new Error("Failed to fetch show-community")
 
-	let filteredUser: UserAPI[] = userData;
+			const data = await response.json();
+			const UserData: UserAPI[] = data.map((user: any) => {
+				return {
+					id: user.id,
+					name: user.name,
+					email: user.email,
+					avatar: user.avatar,
+					doubleAuth: user.doubleAuth,
+					wins: user.wins
+				}
+			})
+			setUserCommunity(UserData);
+
+		} catch(error: any) {
+			console.error(error.message);
+		}
+	}, []);
+
+	useEffect(() => {
+		fetchCommunity();
+	}, [fetchCommunity, userCtx.user])
+
+	let filteredUser: UserAPI[] = userCommunity;
 
 	if (filterOption === 'Most Wins')
-		filteredUser = sortU.winsSortedUser(userData);
+		filteredUser = sortU.winsSortedUser(userCommunity);
 	else if (filterOption === 'Less Wins')
-		filteredUser = sortU.loosesSortedUser(userData);
+		filteredUser = sortU.loosesSortedUser(userCommunity);
 	else if (filterOption === 'Most played games')
-		filteredUser = sortU.mostPlayedGame(userData);
+		filteredUser = sortU.mostPlayedGame(userCommunity);
 	else if (filterOption === 'Less played games')
-		filteredUser = sortU.lessPlayedGame(userData);
+		filteredUser = sortU.lessPlayedGame(userCommunity);
 	else if (filterOption === 'Nickname (a-z)')
-		filteredUser = sortU.alphaOrderNick(userData);
+		filteredUser = sortU.alphaOrderNick(userCommunity);
 	else if (filterOption === 'Nickname (z-a)')
-		filteredUser = sortU.unAlphaOrderNick(userData);
+		filteredUser = sortU.unAlphaOrderNick(userCommunity);
 
 	const displayUsers: UserAPI[] = filteredUser.filter((user) => (
 		user.name.toLowerCase().slice(0, searchInput.length).includes(searchInput.toLowerCase())
 	));
 
-	const fetchBlockUser = async() => {
-		
-	}
 
 	return (
 		<div className={classes.page}>
@@ -70,25 +96,4 @@ export default function Leaderboard() {
 			</div>
 		</div>
 	)
-}
-
-export const loader = async (): Promise<UserAPI[] | Response> => {
-	const response = await fetch('http://localhost:3000/users');
-
-	if (!response.ok) {
-		return json({message: "Could not fetch leaderboard"}, { status: 500 });
-	} else {
-		const data = await response.json();
-		const usersData: UserAPI[] = data.map((user: any) => {
-			return {
-				id: user.id,
-				email: user.email,
-				name: user.name,
-				avatar: user.avatar,
-				doubleAuth: user.doubleAuth,
-				wins: user.wins
-			}
-		})
-		return usersData;
-	}
 }
