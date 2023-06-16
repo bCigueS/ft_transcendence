5cc9c0e3511318939c7bb4e390ea0ef342f103e6
Yangchi <chenade0312@gmail.com>
2023-06-16 11:16:15 +0200

    =

--- backend/app/src/auth/auth.controller.ts
+++ backend/app/src/auth/auth.controller.ts
@@ -30,34 +30,7 @@ export class AuthController {
 
 	@Post('me')
 	@ApiOkResponse({ type: AuthEntity })
-	async token42(
-		@Body() { code }
-	): Promise<any> {
-		var response = {};
-		const url_token = 'https://api.intra.42.fr/oauth/token';
-		const data_token = {
-			grant_type: "authorization_code",
-			client_id: `${process.env.CLIENT_ID}`,
-			client_secret: `${process.env.CLIENT_SECRET}`,
-			code: code,
-			redirect_uri: `${process.env.REDIRECT_URL}`,
-		};
-		console.log(data_token);
-		try 
-		{
-			const t = await this.httpService.post(url_token, data_token).toPromise();
-			response['token'] = t.data;
-		} catch (error) { 
-			error.response.data.status = 403;
-			throw new HttpException(error.response.data , HttpStatus.FORBIDDEN, { cause: error });
-		}
-		// if (response['token']['access_token'])
-		// {
-		// 	const url_data = 'https://api.intra.42.fr/oauth/token/info';
-		// 	const headersRequest = { 'Authorization': `Bearer ${response['token_response']['access_token']}`};
-		// 	const data_response = await this.httpService.post(url_data, {},  {headers: headersRequest}).toPromise();
-		// 	response['token'] = data_response.data;
-		// }
-		return (response);
+	token42( @Body() { code } ): Promise<any> {
+		return this.authService.validateUser(code);
 	}
 }
