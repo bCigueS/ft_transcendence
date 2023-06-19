85351ceee85712099daf42cf6e465f37dc5d9515
sbeylot <sbeylot@student.42.fr>
2023-06-19 11:13:18 +0200

    :construction: Building homepage
    *

--- frontend/app/src/pages/Homepage.tsx
+++ frontend/app/src/pages/Homepage.tsx
@@ -1,8 +1,17 @@
+import LiveGame from '../components/Game/LiveGame';
+import classes from '../sass/pages/Homepage.module.scss'
 
-export default function Homepage() {
+
+const Homepage: React.FC = () => {
 	return (
-		<>
-			<h1>Homepage</h1>
-		</>
+		<div className={classes.main}>
+			<div className={classes.rules}>
+				<h1>Rules of the Game</h1>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore fugit cupiditate adipisci, velit placeat distinctio facere suscipit. Vel cupiditate et adipisci neque voluptas illum cum necessitatibus maiores quis. Alias, nam.</p>
+			</div>
+			<LiveGame/>
+		</div>
 	)
 }
+
+export default Homepage;
