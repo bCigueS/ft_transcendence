482c0f81cd6ea5e017d607a9b2ae1e66d0c44114
Chenade <chenade0312@gmail.com>
2023-06-19 21:13:16 +0200

    otp

--- backend/app/src/auth/auth.controller.ts
+++ backend/app/src/auth/auth.controller.ts
@@ -1,11 +1,10 @@
 import { Body, Controller, Get, HttpException, HttpStatus, Post, Query, Redirect, Res, UseGuards } from '@nestjs/common';
-// import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthEntity } from './entity/auth.entity';
 import { LoginDto } from './dto/login.dto';
 import { OAuth42Strategy } from './strategy/passport.strategy';
-import {OAuth42Guard} from './auth.guard';
+import { OAuth42Guard } from './auth.guard';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
@@ -13,36 +12,44 @@ import { Injectable } from '@nestjs/common';
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService, private readonly httpService: HttpService) {}
+  constructor(private readonly authService: AuthService, private readonly httpService: HttpService) {}
 
-	@Post('login')
-	@ApiOkResponse({ type: AuthEntity })
-	login(@Body() { name, password }: LoginDto) {
-		return this.authService.login(name, password);
-	}
-	
-	@Get('forty-two')
-	@UseGuards(OAuth42Guard, OAuth42Strategy)
-	@ApiOkResponse({ type: AuthEntity })
-	fortyTwo() {
-		return "";
-	}
+  @Post('login')
+  @ApiOkResponse({ type: AuthEntity })
+  login(@Body() { name, password }: LoginDto) {
+    return this.authService.login(name, password);
+  }
 
-	@Post('me')
-	@ApiOkResponse({ type: AuthEntity })
-	token42( @Body() { code } ): Promise<any> {
-		return this.authService.validateUser(code);
-	}
+  @Get('forty-two')
+  @UseGuards(OAuth42Guard, OAuth42Strategy)
+  @ApiOkResponse({ type: AuthEntity })
+  fortyTwo() {
+    return '';
+  }
 
-	@Get('me')
-	@ApiOkResponse({ type: AuthEntity })
-	aboutMe( @Body() { token } ): Promise<any> {
-		return this.authService.aboutMe(token);
-	}
+  @Post('me')
+  @ApiOkResponse({ type: AuthEntity })
+  token42(@Body() { code }: any): Promise<any> {
+    return this.authService.validateUser(code);
+  }
 
-	@Post('2fa/add')
-	@ApiOkResponse({ type: AuthEntity })
-	add2fa( @Body() { token, secret } ): Promise<any> {
-		return this.authService.add2fa(token, secret);
-	}
+  @Get('me')
+  @ApiOkResponse({ type: AuthEntity })
+  aboutMe(@Body() { token }: any): Promise<any> {
+    return this.authService.aboutMe(token);
+  }
+
+  @Post('2fa/add')
+  @ApiOkResponse({ type: AuthEntity })
+  async add2fa(@Body() body: any, @Res() res: any): Promise<any> {
+    const { otpauthUrl } = await this.authService.getTwoFactorAuthenticationCode();
+	res.setHeader('Content-Type', 'image/png'); // Set the content type for the image
+    return this.authService.pipeQrCodeStream(res, otpauthUrl);
+  }
+
+  @Post('2fa/verify')
+  @ApiOkResponse({ type: AuthEntity })
+  async verify2fa(@Body() { userId, token }: any): Promise<any> {
+    return await this.authService.verifyTwoFactorAuthenticationCode(userId, token);
+  }
 }
--- backend/app/src/auth/auth.service.ts
+++ backend/app/src/auth/auth.service.ts
@@ -5,50 +5,51 @@ import { AuthEntity } from './entity/auth.entity';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { toSafeUser } from 'src/users/user.utils';
 import axios from 'axios';
 import fs from 'fs';
 
+import * as speakeasy from 'speakeasy';
+import * as QRCode from 'qrcode';
+import { Response } from 'express'; // Add this import
+
 @Injectable()
 export class AuthService {
-prismaService: any;
-constructor(
-	private prisma: PrismaService, 
-	private jwtService: JwtService, 
-	private readonly httpService: HttpService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private readonly httpService: HttpService
+  ) {}
 
-async login(name: string, password: string): Promise<AuthEntity> {
-	const user = await this.prisma.user.findUnique({ where: { name: name } });
+  async login(name: string, password: string): Promise<AuthEntity> {
+    const user = await this.prisma.user.findUnique({ where: { name: name } });
 
-	if (!user) {
-	throw new NotFoundException(`No user found with username: ${name}`);
-	}
+    if (!user) {
+      throw new NotFoundException(`No user found with username: ${name}`);
+    }
 
-	const isPasswordValid = user.password === password;
+    const isPasswordValid = user.password === password;
 
-	if (!isPasswordValid) {
-		throw new UnauthorizedException('Invalid password');
-	}
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
 
-	return {
-		accessToken: this.jwtService.sign({ userId: user.id }),
-	};
-}
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
 
-async saveImageFromUrl(url: string, filePath: string): Promise<void> 
-{
-	const response = await axios.get(url,{
-	  responseType: 'arraybuffer',
-	});
+  async saveImageFromUrl(url: string, filePath: string): Promise<void> {
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer',
+    });
 
-	const targetPath = `./uploads/${filePath}`;
-  
-	fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
-}
-  
-async registerUser(apiResponse: any)
-{
-	const avatar = `${apiResponse.login}.jpg`; // Specify the desired path where you want to save the image
+    const targetPath = `./uploads/${filePath}`;
+
+    fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
+  }
+
+  async registerUser(apiResponse: any) {
+    const avatar = `${apiResponse.login}.jpg`; // Specify the desired path where you want to save the image
 
     const createUserDto = new CreateUserDto();
     createUserDto.name = apiResponse.displayname;
@@ -59,7 +60,7 @@ async registerUser(apiResponse: any)
 
     const user = await this.prisma.user.create({
       data: {
-		id42: apiResponse.id,
+        id42: apiResponse.id,
         name: createUserDto.name,
         login: createUserDto.login,
         email: createUserDto.email,
@@ -68,62 +69,99 @@ async registerUser(apiResponse: any)
       },
     });
 
-	this.saveImageFromUrl(apiResponse.image.link, avatar);
-    return (user);
-}
+    this.saveImageFromUrl(apiResponse.image.link, avatar);
+    return user;
+  }
 
+  async validateUser(code: string): Promise<any> {
+    var response = {};
+    const url_token = 'https://api.intra.42.fr/oauth/token';
+    const data_token = {
+      grant_type: 'authorization_code',
+      client_id: `${process.env.CLIENT_ID}`,
+      client_secret: `${process.env.CLIENT_SECRET}`,
+      code: code,
+      redirect_uri: `${process.env.REDIRECT_URL}`,
+    };
+    try {
+      const token_data = await this.httpService.post(url_token, data_token).toPromise();
+      response['token'] = token_data.data;
+    } catch (error) {
+      error.response.data.status = 403;
+      throw new HttpException(error.response.data, HttpStatus.FORBIDDEN, { cause: error });
+    }
+    if (response['token']['access_token']) response['user'] = await this.aboutMe(response['token']['access_token']);
+    return response;
+  }
 
-async validateUser(code: string): Promise<any>
-{
-	var response = {};
-	const url_token = 'https://api.intra.42.fr/oauth/token';
-	const data_token = {
-		grant_type: "authorization_code",
-		client_id: `${process.env.CLIENT_ID}`,
-		client_secret: `${process.env.CLIENT_SECRET}`,
-		code: code,
-		redirect_uri: `${process.env.REDIRECT_URL}`,
-	};
-	try 
-	{
-		const token_data = await this.httpService.post(url_token, data_token).toPromise();
-		response['token'] = token_data.data;
+  async aboutMe(token: string): Promise<any> {
+    const url_data = 'https://api.intra.42.fr/v2/me';
+    const headersRequest = { Authorization: `Bearer ${token}` };
+    try {
+      const data_response = await this.httpService.get(url_data, { headers: headersRequest }).toPromise();
+      const user = await this.prisma.user.findFirst({ where: { login: data_response.data.login } });
+      if (!user) await this.registerUser(data_response.data);
+      return data_response.data;
+    } catch (error) {
+      error.status = 403;
+      throw new HttpException(error, HttpStatus.FORBIDDEN, { cause: error });
+    }
+  }
 
-	} catch (error) { 
-		error.response.data.status = 403;
-		throw new HttpException(error.response.data , HttpStatus.FORBIDDEN, { cause: error });
-	}
-	if (response['token']['access_token'])
-		response['user'] = await this.aboutMe(response['token']['access_token']);
-	return (response);
-}
+  async add2fa(token: string, secret: string): Promise<any> {
+    return true;
+  }
 
-async aboutMe(token: string): Promise<any>
-{
-	const url_data = 'https://api.intra.42.fr/v2/me';
-	const headersRequest = { 'Authorization': `Bearer ${token}`};
-	try 
-	{
-		const data_response = await this.httpService.get(url_data,  {headers: headersRequest}).toPromise();
-		const user = await this.prisma.user.findFirst({ where: { login: data_response.data.login } });
-		if (!user)
-			await this.registerUser(data_response.data);
-		return (data_response.data);
-	} catch (error) { 
-		error.status = 403;
-		throw new HttpException(error , HttpStatus.FORBIDDEN, { cause: error });
-	}	
-}
-
-async add2fa(token: string, secret: string): Promise<any>
-{
-	return (true);
-}
-
-async fortyTwo(): Promise<AuthEntity> {
+  async fortyTwo(): Promise<AuthEntity> {
     return {
       accessToken: this.jwtService.sign({ userId: 42 }),
     };
   }
 
+  async getTwoFactorAuthenticationCode()
+  {
+    const secretCode = speakeasy.generateSecret({
+      name: '42ykuo2',
+    });
+	console.log(secretCode);
+    return {
+      otpauthUrl: secretCode.otpauth_url,
+      base32: secretCode.base32,
+    };
+  }
+
+  public verifyTwoFactorAuthenticationCode(userId: string, token: string) {
+	console.log(userId, token);
+	const crypto = require("crypto");
+	const algorithm = "aes-256-cbc"; 
+	const initVector = crypto.randomBytes(16);
+	const message = userId;
+	const Securitykey = crypto.randomBytes(32);
+	const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+
+	console.log("initVector: " + initVector.toString("hex"));
+	console.log("Securitykey: " + Securitykey.toString("hex"));
+
+	let encryptedData = cipher.update(message, "utf-8", "hex");
+	encryptedData += cipher.final("hex");
+	console.log("Encrypted message: " + encryptedData);
+
+	const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+	let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
+	decryptedData += decipher.final("utf8");
+	console.log("Decrypted message: " + decryptedData);
+
+	const verified = speakeasy.totp.verify({
+		secret: userId,
+		encoding: 'base32',
+		token: token,
+	  });
+	  return {
+		result: verified,
+	  };
+  }
+
+  public async pipeQrCodeStream(stream: Response, otpauthUrl: string) {
+    return QRCode.toFileStream(stream, otpauthUrl);
+  }
 }
