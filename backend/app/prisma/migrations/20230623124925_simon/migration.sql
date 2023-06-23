b5daebca4a656299a60bd713c41bebbf99f8b44b
sbeylot <sbeylot@student.42.fr>
2023-06-23 15:03:35 +0200

    :construction: connected status
    *

--- backend/app/prisma/migrations/20230623124925_simon/migration.sql
+++ backend/app/prisma/migrations/20230623124925_simon/migration.sql
@@ -1,7 +1,4 @@
 -- CreateEnum
-CREATE TYPE "GameType" AS ENUM ('PLAYER', 'COMPUTER');
-
--- CreateEnum
 CREATE TYPE "GameState" AS ENUM ('PENDING', 'PLAYING', 'FINISHED');
 
 -- CreateTable
@@ -15,6 +12,9 @@ CREATE TABLE "users" (
     "avatar" TEXT DEFAULT 'default.jpg',
     "doubleAuth" BOOLEAN NOT NULL DEFAULT false,
     "wins" INTEGER NOT NULL DEFAULT 0,
+    "secert" TEXT,
+    "status" INTEGER NOT NULL DEFAULT 0,
+    "token" TEXT,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -40,13 +40,24 @@ CREATE TABLE "blocked" (
 -- CreateTable
 CREATE TABLE "games" (
     "id" SERIAL NOT NULL,
+    "room" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "type" "GameType" NOT NULL,
+    "state" "GameState" NOT NULL DEFAULT 'PENDING',
     "level" INTEGER NOT NULL,
+    "playerSocketIds" TEXT[],
+    "spectatorSocketIds" TEXT[],
     "winnerId" INTEGER,
-    "duration" INTEGER,
 
     CONSTRAINT "games_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SpectatorGame" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "gameId" INTEGER NOT NULL,
+
+    CONSTRAINT "SpectatorGame_pkey" PRIMARY KEY ("userId","gameId")
 );
 
 -- CreateTable
@@ -54,10 +65,37 @@ CREATE TABLE "UserGame" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" INTEGER NOT NULL,
     "gameId" INTEGER NOT NULL,
-    "playerIndex" INTEGER NOT NULL DEFAULT 1,
     "score" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "UserGame_pkey" PRIMARY KEY ("userId","gameId")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" SERIAL NOT NULL,
+    "senderId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "channelId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT DEFAULT 'private',
+
+    CONSTRAINT "Channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ChannelMembership" (
+    "id" SERIAL NOT NULL,
+    "channelId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "ChannelMembership_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -68,6 +106,12 @@ CREATE UNIQUE INDEX "users_name_key" ON "users"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_login_key" ON "users"("login");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "games_room_key" ON "games"("room");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChannelMembership_channelId_userId_key" ON "ChannelMembership"("channelId", "userId");
 
 -- AddForeignKey
 ALTER TABLE "following" ADD CONSTRAINT "following_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -85,7 +129,25 @@ ALTER TABLE "blocked" ADD CONSTRAINT "blocked_blockedId_fkey" FOREIGN KEY ("bloc
 ALTER TABLE "games" ADD CONSTRAINT "games_winnerId_fkey" FOREIGN KEY ("winnerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "SpectatorGame" ADD CONSTRAINT "SpectatorGame_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SpectatorGame" ADD CONSTRAINT "SpectatorGame_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "UserGame" ADD CONSTRAINT "UserGame_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "UserGame" ADD CONSTRAINT "UserGame_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelMembership" ADD CONSTRAINT "ChannelMembership_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChannelMembership" ADD CONSTRAINT "ChannelMembership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
