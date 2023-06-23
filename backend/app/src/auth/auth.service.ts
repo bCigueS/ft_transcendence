cc077d8a28df5e00fa7a5622d0d7f6ea135b8f45
Chenade <chenade0312@gmail.com>
2023-06-23 12:55:59 +0200

    :contruction: wrap the function in utils
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch ykuo/backend
    # Your branch is up to date with 'origin/ykuo/backend'.
    #
    # Changes to be committed:
    #	modified:   backend/app/src/auth/auth.service.ts
    #	new file:   backend/app/src/auth/auth.utils.ts
    #	deleted:    backend/app/src/auth/jwt-auth.guard.ts
    #	modified:   backend/app/src/users/users.controller.ts
    #

--- backend/app/src/auth/auth.service.ts
+++ backend/app/src/auth/auth.service.ts
@@ -5,11 +5,13 @@ import { AuthEntity } from './entity/auth.entity';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import * as AuthUtils from './auth.utils';
 import axios from 'axios';
 import fs from 'fs';
 import * as jwt from 'jsonwebtoken';
 import * as speakeasy from 'speakeasy';
 import * as CryptoJS from 'crypto-js';
+import { authenticate } from 'passport';
 
 @Injectable()
 export class AuthService {
@@ -124,43 +126,11 @@ export class AuthService {
     const user = await this.prisma.user.findUnique({ where: { id: userId } });
     if (!user)
     	throw new NotFoundException(`User with ${userId} does not exist.`);
-    const verified = speakeasy.totp.verify({
-      secret: user.secert,
-      encoding: 'base32',
-      token: code,
-    })
-	console.log("Verify: ", verified);
+    const verified = AuthUtils.verifyTwoFactorAuthenticationCode(code, user.secert);
 	if (verified)
 	{
-		try {
-			const url_data = 'https://api.intra.42.fr/v2/me';
-			const token = CryptoJS.AES.decrypt(user.token, `${process.env.NODE_ENV}`).toString(CryptoJS.enc.Utf8);
-			const headersRequest = { Authorization: `Bearer ${token}` };
-			const data_response = await this.httpService.get(url_data, { headers: headersRequest }).toPromise();
-			if (user)
-				await this.prisma.user.update({ where: { id42: data_response.data['id'] }, data: {status: 1},});
-			
-			const accessToken = jwt.sign({
-				accessToken: token,
-				userId: user.id
-			}, `${process.env.NODE_ENV}`, { expiresIn: '1h' });
-			
-			return {
-				accessToken: accessToken,
-				userId: user.id,
-				doubleAuth: user.doubleAuth,
-				id: data_response.data['id'],
-				email: data_response.data['email'],
-				login: data_response.data['login'],
-				displayname: data_response.data['displayname'],
-				image: data_response.data['image_url'],
-				first_name: data_response.data['first_name'],
-				last_name: data_response.data['last_name'],
-			};
-		} catch (error) {
-			error.status = 403;
-			throw new HttpException(error, HttpStatus.FORBIDDEN, { cause: error });
-		}
+		let response = AuthUtils.getUserData(user);
+		response['accessToken'] = AuthUtils.signToken(user.id, user.token);
 	}
 	else
 		await this.prisma.user.update({ where: { id: userId}, data: {status: 0},});
