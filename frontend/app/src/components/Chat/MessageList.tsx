e4d959c98c40ee538c2a347f07e9c878b60acfab
olivia <oliviawalsh073@gmail.com>
2023-06-28 15:09:22 +0200

    :construction: Do not display blocked users chat
    *

--- frontend/app/src/components/Chat/MessageList.tsx
+++ frontend/app/src/components/Chat/MessageList.tsx
@@ -13,6 +13,7 @@ type JoinResponse = {
 const MessageList: React.FC<{send: (content: string, channelId: number) => {}, chat: Channel, chats: Channel[], msgs: MessageAPI[], onDelete: (message: MessageAPI) => void, onJoin: (joinData: JoinChannelDTO) => Promise<JoinResponse>}> = ({ send, chat, chats, msgs, onDelete, onJoin }) => {
 
     const [ messages, setMessages ] = useState<MessageAPI[]>(chat.messages);
+	const [ senderBlocked, setSenderBlocked ] = useState(false);
 	const messageInput = useRef<HTMLInputElement>(null);
 	const userCtx = useContext(UserContext);
 
@@ -27,7 +28,6 @@ const MessageList: React.FC<{send: (content: string, channelId: number) => {}, c
     useEffect(() => {
         setMessages(chat.messages);
     }, [chat, chats, msgs]);
-
     
     const isMine = (message: MessageAPI) => {
 		if (message.senderId === userCtx.user?.id)
